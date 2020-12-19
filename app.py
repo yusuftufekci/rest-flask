0943a0dfc4ce041b9bfce7248b1c493784afb3e6
yusuftufekci <yusuftufekci96@gmail.com>
2020-12-20 01:24:36 +0300

    change2

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ def signup_post():
     user = User.query.filter_by(name=username).first() # if this returns a user, then the email already exists in database
 
     if user: # if a user is found, we want to redirect back to signup page so user can try again
-        return "Kullanıcı mevcut, tekrar dene"
+        return "kullanici mevcut, tekrar dene"
 
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
     new_user = User(id=id, name=username, email=email, password=generate_password_hash(password, method='sha256'))
@@ -179,12 +179,12 @@ def login_post():
     # check if the user actually exists
     # take the user-supplied password, hash it, and compare it to the hashed password in the database
     if not user or not check_password_hash(user.password, password):
-        return "giriş başarısız"
+        return "giris basarisiz"
         # if the user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
 
-    return "Giriş başarılı"
+    return "giris basarili"
 
 @app.route('/api/users/<id>', methods=['GET', 'POST'])
 def home(id):
